dde40e660409e9a8e73459269e7b28a9e835eb82
Iwantroca <mohitnotiyal@yahoo.com>
2022-01-14 17:03:31 +0530

    Add basic page structure

--- dist/main.js
+++ dist/main.js
@@ -3,8 +3,14 @@ var __webpack_exports__ = {};
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-console.log("hei");
+const mainContent = document.querySelector("#content");
+function component() {
+  const element = document.createElement("div");
+  element.textContent = "hey bro";
+  return element;
+}
+mainContent.appendChild(component());
 
 /******/ })()
 ;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVzdGF1cmFudC1wYWdlLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKFwiaGVpXCIpO1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVzdGF1cmFudC1wYWdlLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnN0IG1haW5Db250ZW50ID0gZG9jdW1lbnQucXVlcnlTZWxlY3RvcihcIiNjb250ZW50XCIpO1xuZnVuY3Rpb24gY29tcG9uZW50KCkge1xuICBjb25zdCBlbGVtZW50ID0gZG9jdW1lbnQuY3JlYXRlRWxlbWVudChcImRpdlwiKTtcbiAgZWxlbWVudC50ZXh0Q29udGVudCA9IFwiaGV5IGJyb1wiO1xuICByZXR1cm4gZWxlbWVudDtcbn1cbm1haW5Db250ZW50LmFwcGVuZENoaWxkKGNvbXBvbmVudCgpKTtcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log("hei");
+const mainContent = document.querySelector("#content");
+console.log(mainContent);
+
+function createHeader() {
+  const header = document.createElement("header");
+  header.appendChild(createHeadTitle());
+  header.appendChild(createNav());
+  return header;
+}
+
+function createHeadTitle() {
+  const headTitle = document.createElement("h1");
+  headTitle.textContent = "My Lonely Cafe";
+  headTitle.style.textAlign = "center";
+  return headTitle;
+}
+function createNav() {
+  const navBar = document.createElement("nav");
+  const home = document.createElement("p");
+  const about = document.createElement("p");
+  const menu = document.createElement("p");
+  home.textContent = "home";
+  about.textContent = "about";
+  menu.textContent = "menu";
+  navBar.append(home);
+  navBar.append(about);
+  navBar.append(menu);
+  return navBar;
+}
+mainContent.appendChild(createHeader());
